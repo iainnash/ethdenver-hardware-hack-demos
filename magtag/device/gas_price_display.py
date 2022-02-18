7705b5c2833726aee2d405cfadd2bc65452aac43
Iain <iain@zora.co>
2022-02-18 12:47:59 -0700

    add font and gas price display

--- magtag/device/gas_price_display.py
+++ magtag/device/gas_price_display.py
@@ -12,7 +12,7 @@ from adafruit_magtag.magtag import MagTag
 DAILY_UPDATE_HOUR = 20
 
 # Set up where we'll be fetching data from
-DATA_SOURCE = "https://gas-monitor.isiain.workers.dev/get-gas-level"
+DATA_SOURCE = "http://gas-monitor.isiain.workers.dev/get-gas-level"
 SAFE_GAS_PRICE_LOCATION = ['SafeGasPrice']
 PROPOSE_GAS_PRICE_LOCATION = ['ProposeGasPrice']
 FAST_GAS_PRICE_LOCATION = ['FastGasPrice']
@@ -51,21 +51,18 @@ magtag.add_text(
     is_data=False
 )
 
-magtag.graphics.qrcode(b"https://covidtracking.com/data",
-                       qr_size=2, x=240, y=70)
-
 magtag.peripherals.neopixels.brightness = 0.1
 magtag.peripherals.neopixel_disable = False # turn on lights
 magtag.peripherals.neopixels.fill(0x0F0000) # red!
 
-magtag.get_local_time()
+#magtag.get_local_time()
 try:
-    now = time.localtime()
-    print("Now: ", now)
+    #now = time.localtime()
+    #print("Now: ", now)
 
     # display the current time since its the last-update
-    updated_at = "%d/%d\n%d:%02d" % now[1:5]
-    magtag.set_text(updated_at, 6, False)
+    #updated_at = "%d/%d\n%d:%02d" % now[1:5]
+    #magtag.set_text(updated_at, 6, False)
 
     # get data from the Covid Tracking Project
     value = magtag.fetch()
@@ -78,17 +75,9 @@ except (ValueError, RuntimeError) as e:
 
 time.sleep(2) # let screen finish updating
 
-# we only wanna wake up once a day, around the event update time:
-event_time = time.struct_time((now[0], now[1], now[2],
-                               DAILY_UPDATE_HOUR, 0, 0,
-                               -1, -1, now[8]))
-# how long is that from now?
-remaining = time.mktime(event_time) - time.mktime(now)
-if remaining < 0:             # ah its aready happened today...
-    remaining += 24 * 60 * 60 # wrap around to the next day
-remaining_hrs = remaining // 3660
-remaining_min = (remaining % 3600) // 60
-print("Gonna zzz for %d hours, %d minutes" % (remaining_hrs, remaining_min))
+remaining = 60 # update 60 seconds
+time.sleep(remaining)
 
 # Turn it all off and go to bed till the next update time
-magtag.exit_and_deep_sleep(remaining)
+#magtag.exit_and_deep_sleep(remaining)
+
