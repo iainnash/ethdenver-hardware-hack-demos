e8cfec07f733eeedceb1a70beda3aaabac71e9d6
Iain <iain@zora.co>
2022-02-20 12:30:58 -0700

    add lcd screen example

---
+++ esp32-display-lcd/src/display_config.h
@@ -0,0 +1,16 @@
+// go to libdeps and select User_Setup_select 18
+
+// #define ST7789_DRIVER
+#define ST7789_2_DRIVER
+
+#define TFT_RST 22 // Brown
+#define TFT_DC 23 // Blue
+#define TFT_CS 2 // Yellow
+#define TFT_SCLK 17 // Orange
+#define TFT_MOSI 13 // Green
+#define TFT_BL 37 // Gray
+#define TFT_BACKLIGHT_ON 1
+
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 320
+
